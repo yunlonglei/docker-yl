6bcfb58345b924cbeafc06f26b29fefb71a9a91b
leiyunlong <1175333149@qq.com>
2020-09-21 18:01:57 +0800

    获取启动参数

--- src/main/java/com/lsjt/docker_yl/controller/HelloController.java
+++ src/main/java/com/lsjt/docker_yl/controller/HelloController.java
@@ -1,8 +1,13 @@
 package com.lsjt.docker_yl.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+
 /**
  * @author leiyunlong
  * @version 1.0
@@ -11,14 +16,17 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @Slf4j
 public class HelloController {
+    @Autowired
+    ApplicationArguments applicationArguments;
     /**
      * hello world测试接口
      * @return 字符串
      */
     @GetMapping("hello")
     public String hello1(){
+        System.out.println(Arrays.toString(applicationArguments.getSourceArgs()));
         log.info("访问hello接口=====");
-        return "hello, Add CI / CD here in docker,thanks!3 ";
+        return "hello, Add CI / CD here in docker,thanks!4 ";
     }
     /**
      * hello world测试接口
