59d6271d40cbcdf68a38b58a34fff24f5278a02d
yunlonglei <yunlonglei0@gmail.com>
2021-10-20 18:27:29 +0800

    连接Docker redis

--- src/main/java/com/lsjt/docker_yl/controller/HelloController.java
+++ src/main/java/com/lsjt/docker_yl/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.lsjt.docker_yl.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
@@ -18,23 +19,35 @@ import java.util.Arrays;
 public class HelloController {
     @Autowired
     ApplicationArguments applicationArguments;
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
     /**
      * hello world测试接口
+     *
      * @return 字符串
      */
     @GetMapping("hello")
-    public String hello1(){
+    public String hello1() {
         System.out.println(Arrays.toString(applicationArguments.getSourceArgs()));
         log.info("访问hello接口=====");
         return "hello, Add CI / CD here in docker,thanks!4 ";
     }
+
     /**
      * hello world测试接口
+     *
      * @return 字符串
      */
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "hello";
     }
 
+    @GetMapping("/count")
+    public String incountdex() {
+        // 使用redis计数累加
+        Long count = stringRedisTemplate.opsForValue().increment("count");
+        return "有" + count + "人访问了我空间";
+    }
 }
